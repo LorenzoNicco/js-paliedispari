e4a3aa1d5b863b8941f2b525884727635e431151
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-01-10 16:08:32 +0100

    Add steps commentati in javascript

---
+++ Pari e dispari/js/script.js
@@ -0,0 +1,11 @@
+/*
+    STEPS:
+        1. Chiedere all'utente di inserire un numero pari o dispari da uno a cinque
+            1A. Creare un prompt che salvi la risposta in una variabile.
+        2. Dichiarare una variabile prima della funzione, alla quale assoceremo il numero estratto per il computer.
+        3. Creare una funzione che estrae un numero casuale tra uno e cinque per il computer.        
+        4. Usare una funzione per stabilire se il risultato della somma sia pari o dispari.
+            4A. Fare la somma dei numeri dell'utente e del computer.
+            4B. Al suo interno mettiamo la condizione che verifichi la disparità o meno 
+            4C. Dichiarare se ha vinto l'utente o il computer.
+*/

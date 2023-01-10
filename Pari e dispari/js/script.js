6c5dabf063b3a79b2954b5a506318326c1705035
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-01-10 16:46:39 +0100

    Add due soluzioni per l'esercizio

--- Pari e dispari/js/script.js
+++ Pari e dispari/js/script.js
@@ -9,3 +9,61 @@
             4B. Al suo interno mettiamo la condizione che verifichi la disparità o meno 
             4C. Dichiarare se ha vinto l'utente o il computer.
 */
+const userWord = prompt("Pari o dispari?");
+console.log("userWord", userWord, typeof userWord);
+
+const userNumber = parseInt(prompt("Scrivi un numero da uno a cinque"));
+console.log("userNumber", userNumber, typeof userNumber);
+
+// Prima soluzione
+let computerNumber;
+
+function computerNumberGenerator () {
+    computerNumber = Math.floor(Math.random() * 5) + 1;
+
+    return computerNumber;
+}
+
+const cpuNumber = computerNumberGenerator();
+console.log("cpuNumber", cpuNumber, typeof cpuNumber);
+
+// function pariDispari (num1, num2) {
+//     const somma = num1 + num2;
+//     console.log("somma", somma, typeof somma);
+
+//     if (somma % 2 == 0) {
+//         alert("Vince pari");
+//         console.log("Vince pari");
+//     }
+//     else {
+//         alert("Vince dispari");
+//         console.log("Vince dispari");
+//     }
+// }
+
+// const risultato = pariDispari(userNumber, cpuNumber);
+
+// Seconda soluzione
+function pariDispari (num1, num2) {
+    const somma = num1 + num2;
+    console.log("somma", somma, typeof somma);
+
+    if (somma % 2 == 0) {
+        return "pari";
+    }
+    else {
+        return "dispari";
+    }
+}
+
+const risultato = pariDispari(userNumber, cpuNumber);
+console.log("risultato", risultato, typeof risultato);
+
+if (userWord === risultato) {
+    alert("Hai vinto!");
+    console.log("Hai vinto!");
+}
+else {
+    alert("Hai perso");
+    console.log("Hai perso");
+}
